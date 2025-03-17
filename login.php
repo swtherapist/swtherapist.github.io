bfb8933e29b6d6956ee1d4a8769b3aeaf612230e
jayeonin <kimseungil@gmail.com>
2025-03-17 19:08:25 +0900

    login password is modified

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$conn = new mysqli('localhost', 'username', 'password', 'database');
+$conn = new mysqli('82.180.152.103', 'u836162453_jayeonin', 'dPtnwjd00!', 'u836162453_db');
 
 if ($conn->connect_error) {
     die(json_encode(['success' => false, 'message' => 'Database connection failed.']));
